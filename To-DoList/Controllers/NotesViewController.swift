e40dc26eeed0fa55929c194ab3c54ea735788d8c
sujata-shyam <sujata.shyam.123@gmail.com>
2019-12-03 09:04:18 +0530

    Basic Functionalities except Due Date and Reminder achieved in Core Data

--- To-DoList/Controllers/NotesViewController.swift
+++ To-DoList/Controllers/NotesViewController.swift
@@ -12,62 +12,147 @@ import CoreData
 class NotesViewController: UIViewController
 {
     @IBOutlet weak var titleTextView: UITextView!
- 
+    @IBOutlet weak var noteTextView: UITextView!
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
-    var selectedCategory:Category? {
-        didSet
-        {
-            loadItems()
-        }
-    }
+    var globalNote:Notes? = nil
+    var globalNoteText:String? = nil
+    var isNew = true
+    
+//    var selectedCategory:Category? {
+//        didSet
+//        {
+//            loadUpdateNotes()
+//        }
+//    }
+    
+    var selectedCategory = Category()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        loadUpdateNotes()
+        setContent()
     }
 
+//    func saveChanges() {
+//        let newNote = Notes(context: self.context)
+//
+//        if(isNew)
+//        {
+//            newNote.parentCategory = self.selectedCategory
+//            newNote.note = noteTextView.text
+//            isNew = false
+//
+//            self.saveNotes()
+//        }
+//        else
+//        {
+//            loadUpdateNotes(isUpdate:true)
+//        }
+//    }
+    
     @IBAction func btnSaveTapped(_ sender: UIButton)
     {
+        //saveChanges()
+
+        let newNote = Notes(context: self.context)
         
+        if(isNew)
+        {
+            newNote.parentCategory = self.selectedCategory
+            newNote.note = noteTextView.text
+            isNew = false
+            
+            self.saveNotes()
+        }
+        else
+        {
+            loadUpdateNotes(isUpdate:true)
+        } //navigationController?.popViewController(animated: true)
     }
     
     @IBAction func btnCancelTapped(_ sender: UIButton)
     {
-        
+        noteTextView.text = globalNote?.note
     }
     
     @IBAction func btnDeleteTapped(_ sender: UIButton)
     {
+        //Delete the row from the data source
+        if globalNote != nil
+        {
+            context.delete(globalNote!)
+            saveNotes()
+            noteTextView.text = nil
+        }
     }
     
+    @IBAction func btnExitTapped(_ sender: UIButton)
+    {
+        //Delete the row from the data source
+        if globalNote != nil
+        {
+            context.delete(globalNote!)
+            saveNotes()
+        }
+    }
     //MARK: - Model Manipulation Methods
     
-    func saveItems()
+    func saveNotes()
     {
-        do
+        if context.hasChanges
         {
-            try context.save()
-        }
-        catch
-        {
-            print("Error saving notes:\(error)")
+            do
+            {
+                try context.save()
+            }
+            catch
+            {
+                print("Error saving notes:\(error)")
+            }
         }
     }
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil)
+    func loadUpdateNotes(with request: NSFetchRequest<Notes> = Notes.fetchRequest(), predicate: NSPredicate? = nil, isUpdate:Bool = false)
     {
-        request.predicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
-        do
+        if(isUpdate == false)
         {
-            let notes = try context.fetch(request)
-            print(notes)
+            request.predicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory.name!)
+            do
+            {
+                let notes = try context.fetch(request)
+                if(notes.count>0)
+                {
+                    isNew = false
+                    globalNote = notes[0]
+                }
+            }
+            catch
+            {
+                print("Error fetching data from context:\(error)")
+            }
         }
-        catch
+        else
         {
-            print("Error fetching data from context:\(error)")
+            print(globalNote)
+            globalNote?.setValue(noteTextView.text, forKey: "note")
+            saveNotes()
         }
     }
     
-    
+    func setContent()
+    {
+        titleTextView.text = selectedCategory.name
+        if(globalNote != nil)
+        {
+            if let note = globalNote?.note
+            {
+                noteTextView.text = note
+            }
+        }
+
+    }
 }
